6a0814a00992d6000b95ce651be238a5482cc764
fktona <adetona.fk@gmail.com>
2025-01-19 07:10:42 +0100

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,18 +40,14 @@ export default function RootLayout({
       <body
         className={`${ppInter.variable} ${ppbyte.variable} overflow-x-hidden antialiased isolate`}
       >
-        {!show && (
-          <>
-            <iframe
-              src="https://my.spline.design/particlenebulacopy-5f3382e8ff624a908a21b10a995d4d52/"
-              frameBorder="0"
-              width="100%"
-              height="100%"
-              className="fixed top-0 blur-md left-0 w-full h-full "
-            ></iframe>
-            <div className="fixed top-0 left-0 w-full h-full bg-black/60"></div>
-          </>
-        )}
+        <iframe
+          src="https://my.spline.design/particlenebulacopy-5f3382e8ff624a908a21b10a995d4d52/"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+          className="fixed top-0 blur-md left-0 w-full h-full "
+        ></iframe>
+        <div className="fixed top-0 left-0 w-full h-full bg-black/60"></div>
 
         <AppProvider>
           <div className="min-h-dvh   pt-2 max-w-screen-2xl mx-auto flex flex-col  lg:gap-8  justify-between   w-full">
