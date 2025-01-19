6efd25133fc1f652956a4388420b8adbc6ecff4c
fktona <adetona.fk@gmail.com>
2025-01-19 11:29:38 +0100

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,11 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-import AppProvider, { useAppContext } from "./context/context";
+import AppProvider from "./context/context";
 import { AnimatePresence } from "framer-motion";
 import { ppInter, ppbyte } from "./fonts/font";
 import React from "react";
+import { Suspense } from "react";
 import Wrapper from "./wrapper";
 
 export const meta: Metadata = {
@@ -26,15 +27,17 @@ export default function RootLayout({
         className={`${ppInter.variable} ${ppbyte.variable} overflow-x-hidden antialiased isolate`}
       >
         <AppProvider>
-          <Wrapper>
-            <div className="min-h-dvh pt-2 max-w-screen-2xl mx-auto flex flex-col lg:gap-8 justify-between w-full">
-              <Navbar />
-              <main className="grow z-20 scale-90">{children}</main>
-              <AnimatePresence>
-                <Footer />
-              </AnimatePresence>
-            </div>
-          </Wrapper>
+          <Suspense fallback={<div>...</div>}>
+            <Wrapper>
+              <div className="min-h-dvh pt-2 max-w-screen-2xl mx-auto flex flex-col lg:gap-8 justify-between w-full">
+                <Navbar />
+                <main className="grow z-20 scale-90">{children}</main>
+                <AnimatePresence>
+                  <Footer />
+                </AnimatePresence>
+              </div>
+            </Wrapper>
+          </Suspense>
         </AppProvider>
       </body>
     </html>
